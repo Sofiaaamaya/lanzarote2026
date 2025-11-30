d259631bec973295339168551d51c6adbda7fa6b
Sofiaaamaya <soofiimaya@gmail.com>
2025-11-30 18:00:52 +0000

    30/11

--- includes/paginas/horario.controller.php
+++ includes/paginas/horario.controller.php
@@ -1,4 +1,8 @@
 <?php
+    if (Campo::val('modo') == 'ajax')
+        define('BOTON_ENVIAR',"<button onclick=\"fetchJSON('/usuarios/".Campo::val('oper')."/". Campo::val('id') ."?modo=ajax','formulario');return false\" class=\"btn btn-primary\">". Idioma::lit('enviar'.Campo::val('oper'))."</button>");
+    else
+        define('BOTON_ENVIAR',"<button type=\"submit\" class=\"btn btn-primary\">". Idioma::lit('enviar'.Campo::val('oper'))."</button>");
 
     class HorarioController{
 
@@ -9,7 +13,7 @@
 
             $contenido = "";
 
-
+            self::inicializacion_campos();
         }
 
 
