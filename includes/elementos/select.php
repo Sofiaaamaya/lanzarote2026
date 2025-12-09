87a927cdad0aca72ad9cf5bc702a78c7759727db
sofia <soofiimaya@gmail.com>
2025-12-09 09:46:07 +0000

    antes del examen lol

--- includes/elementos/select.php
+++ includes/elementos/select.php
@@ -18,6 +18,8 @@
             }
         }
 
+
+
     function pintar()
     {
 
@@ -54,3 +56,52 @@
     }
 
     }
+
+
+
+
+    
+/*
+
+class cb extends Elemento{
+    function __consruct($datos=[]){
+        $datos=['esqueleto'] = False;
+        $datos=['type'] = 'checkbox';
+        $this->value = $datos['value'] ?? '1';
+        parent::__construct($datos);
+
+    }
+    function validar(){
+    $this->error = False;
+    }
+
+    function pintar(){
+    $this->previo_pintar();
+    $checked = (Campo::val($this->nombre) == $this->value) ? 'checked' : '';
+
+    
+
+    $cb_html = "
+        <div class="form-check">
+            <input>
+                <class="form-check_input {$this->style}"
+                type="{$this->type}
+                value="{$this->value}
+                nombre="{$this->nombre}
+                id="{$this->nombre}
+                {$checked} {$this->disabled}
+                <label class="form-check-label" form="id{$this->nombre}" 
+                ".Idioma::lit($this->nombre)
+                </label>
+                {$this->literal_error}
+            </input>
+        </div>
+        ";
+        }
+    
+        return $cb_html;
+
+
+}
+
+*/ 
